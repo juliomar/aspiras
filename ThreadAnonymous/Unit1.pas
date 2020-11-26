0bc378b7647e661839ce9d98a0dfa826989bf970
Juliomar Marchetti <juliomar_m@yahoo.com.br>
2020-11-26 15:21:59 -0300

    thread

---
+++ ThreadAnonymous/Unit1.pas
@@ -0,0 +1,41 @@
+unit Unit1;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
+
+type
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Memo1: TMemo;
+    procedure Button1Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  TThread.CreateAnonymousThread(
+    procedure
+    begin
+      TThread.Sleep(5000);
+      TThread.Synchronize(TThread.CurrentThread,
+      procedure
+      begin
+       Memo1.Lines.Add('Nova linha');
+      end);
+    end).Start;
+end;
+
+end.
